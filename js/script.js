af52bd168a4d8216265fdabdbce518b3807687e9
Fajar Fadillah Agustian <fajaragustian979@gmail.com>
2020-08-02 20:18:33 +0700

    revisi bagian header, hero, dan service

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
 window.addEventListener("scroll", function () {
   const header = document.querySelector("header");
-  header.classList.toggle("sticky", scrollY > 50);
+  header.classList.toggle("sticky", scrollY > 30);
 });
