cdb8784ea70b3ef03c3a4e908b672701beae39f7
Fajar Fadillah Agustian <fajaragustian979@gmail.com>
2020-08-03 20:03:21 +0700

    mengedit footer

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
+// sticky navbar
 window.addEventListener("scroll", function () {
   const header = document.querySelector("header");
   header.classList.toggle("sticky", scrollY > 30);
 });
+
+// footer copyright year
+const yearEl = document.querySelector(".year");
+const date = new Date();
+const year = date.getFullYear();
+yearEl.innerHTML = year;
